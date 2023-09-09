7b460065abf141cb7cf6e8d20f158e195430fcbc
Deepak Guptha <vzan2012@gmail.com>
2023-09-09 23:20:03 +0530

    feat: aborting queries

--- src/util/http.jsx
+++ src/util/http.jsx
@@ -1,9 +1,12 @@
-export const fetchEvents = async (searchTerm) => {
+export const fetchEvents = async ({ signal, searchTerm }) => {
+  console.log(searchTerm);
   let url = `http://localhost:3000/events`;
 
   if (searchTerm) url += `?search=${searchTerm}`;
 
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    signal,
+  });
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
